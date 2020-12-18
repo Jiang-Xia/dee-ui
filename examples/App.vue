08c163335f7599355d623ac32c26a9be1697d4bb
JiangChuanJun <JiangChuanJun@deepaint.cn>
2020-12-18 17:18:51 +0800

    2

--- examples/App.vue
+++ examples/App.vue
@@ -19,11 +19,14 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  .el-main-{
+    padding: 5px;
+  }
 }
 
 #nav {
+  text-align: center;
   padding: 10px;
   a {
     font-weight: bold;
--- examples/views/index/index.vue
+++ examples/views/index/index.vue
@@ -1,21 +1,49 @@
 <template>
   <div>
-    <!-- <dee-desp />
-    <dee-long-text />
-    <dee-short-text />
-    <dee-multiple-choice />
-    <dee-multiple-dropdown />
-    <dee-single-choice />
-    <dee-single-dropdown /> -->
-    <dee-preview-model />
+    <section v-for="(item,index) in group_list" :key="item.id+index">
+      <h2 class="dim-heading">{{ item.name }}</h2>
+      <dee-preview-model :field-temp="item.items" :dim-data="{}" />
+    </section>
   </div>
 </template>
 <script>
+import FieldTemp from './睡眠呼吸监测.json'
 export default {
   data() {
     return {
-      data: ''
+      previewVisible: false,
+      group_list: []
+    }
+  },
+  created() {
+    this.init()
+  },
+  methods: {
+    init() {
+      const { group_list } = FieldTemp
+      this.previewVisible = true
+      this.group_list = group_list
+      group_list.forEach((v, i) => {
+        v.items.forEach((v2, i2) => {
+          if (v2.type === 'long_text') {
+            console.log('==============', v2)
+          } else {
+            // console.log('--------------', v2)
+          }
+        })
+      })
+      // console.log(group_list)
     }
   }
 }
 </script>
+<style lang="scss">
+@import "~@/styles/scss/_mixins";
+.dim-heading{
+    margin-top: 1rem;
+    font-weight: 600;
+    font-size: 17px;
+    color: $main-color;
+    // text-indent: 1em;
+  }
+</style>
