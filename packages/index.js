50a8748ae6eda7b54389774d89288cb3d477e3c1
jiangxia <963798512@qq.com>
2020-12-20 21:32:19 +0800

    1

--- packages/index.js
+++ packages/index.js
@@ -5,7 +5,6 @@ import ShortText from './short-text'
 import MultipleChoice from './multiple-choice'
 import MultipleDropdown from './multiple-dropdown'
 import SingleChoice from './single-choice'
-
 import SingleDropdown from './single-dropdown'
 import PreviewModel from './preview-model'
 import './style/common.scss'
