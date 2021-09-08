788d8eee87bcbb1cfca04f9ead9954e46d290416
Xia <51073570+Jiang-Xia@users.noreply.github.com>
2021-09-08 23:59:21 +0800

    Update babel.config.js
    测试分支222

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: 酱
+ * @LastEditors: 酱
+ * @Date: 2021-09-08 23:57:49
+ * @LastEditTime: 2021-09-08 23:59:08
+ * @Description: 
+ * @FilePath: \dee-ui\babel.config.js
+ */
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
 }
+// 测试分支
